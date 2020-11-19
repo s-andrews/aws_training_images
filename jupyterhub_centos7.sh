0d1152a07da62198e340ddad08a9a52a8c03602b
Simon Andrews <simon@proteo.me.uk>
2020-11-19 14:44:32 +0000

    Add sudo and jupyterlab

--- jupyterhub_centos7.sh
+++ jupyterhub_centos7.sh
@@ -1,14 +1,14 @@
 # Install python
-yum -y install python3
+sudo yum -y install python3
 
 # Install Development Tools so we can compile packages
-yum -y groupinstall "Development Tools"
+sudo yum -y groupinstall "Development Tools"
 
 # Create the user we're going to use and make sure their home
 # directory is also created since useradd doesn't do this by
 # default
 
-useradd -m -d /home/student -s /bin/bash student
+sudo useradd -m -d /home/student -s /bin/bash student
 
 # Set their password since we don't want a hardcoded password.  To give some
 # randomness we'll use the instance ID of the server as the initial password
@@ -19,43 +19,46 @@ useradd -m -d /home/student -s /bin/bash student
 # will only work on the actual AWS image.  This bit will obviously fail if you're
 # not on AWS
 
-yum -y install curl
+sudo yum -y install curl
 sudo sh -c 'curl -s http://169.254.169.254/latest/meta-data/instance-id | passwd --stdin student'
 
 # Install the notebook packages
-pip3 install notebook
+sudo pip3 install notebook
 
 # Install the hub
-pip3 install jupyterhub
+sudo pip3 install jupyterhub
 
 # This needs npm to get configurable http proxy
 curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
-yum install -y nodejs
-npm install -g configurable-http-proxy
+sudo yum install -y nodejs
+sudo npm install -g configurable-http-proxy
 
 # Put some of the most common packages in place. It looks like
 # seaborn already depends on all of the other relevant stuff so
 # this covers pretty much everything on its own.
-pip3 install seaborn
+sudo pip3 install seaborn
+sudo pip3 install jupyterlab
 
 # Since we're on AWS we're going to end up using plain HTTP for our
 # connections.  Jupyterhub will give us a warning on every login about
 # this which we can't do anything about, so let's hide it
-perl -i.bak -pe "s/<p id='insecure-login-warning' class='hidden'>/<p class='hidden'>/" /usr/local/share/jupyterhub/templates/login.html
+sudo perl -i.bak -pe "s/<p id='insecure-login-warning' class='hidden'>/<p class='hidden'>/" /usr/local/share/jupyterhub/templates/login.html
 
 # We can also swap out the logo so we can brand this with our logo
-cp images/bioinformatics_logo_225x80.png /usr/local/share/jupyterhub/static/images/jupyterhub-80.png
+sudo cp images/bioinformatics_logo_225x80.png /usr/local/share/jupyterhub/static/images/jupyterhub-80.png
 
 # Start the actual server. We do this by creating a systemd 
 # unit for it and then starting that.
 
+sudo sh -e '
 echo "#!/bin/bash
 export PATH=$PATH:/usr/local/bin/
 jupyterhub --port=80 > /var/log/jupyterhub 2>&1
-" > /usr/local/sbin/start_jupyterhub.sh
+" > /usr/local/sbin/start_jupyterhub.sh'
 
-chmod 755 /usr/local/sbin/start_jupyterhub.sh
+sudo chmod 755 /usr/local/sbin/start_jupyterhub.sh
 
+sudo sh -e '
 echo "[Unit]
 Description=Starts the jupyterhub service
 After=network.target
@@ -67,11 +70,11 @@ TimeoutStartSec=0
 
 [Install]
 WantedBy=default.target
-" > /etc/systemd/system/jupyterhub.service
+" > /etc/systemd/system/jupyterhub.service'
 
 # Now register and start the service
-systemctl daemon-reload
-systemctl enable jupyterhub
-systemctl start jupyterhub
+sudo systemctl daemon-reload
+sudo systemctl enable jupyterhub
+sudo systemctl start jupyterhub
 
 
