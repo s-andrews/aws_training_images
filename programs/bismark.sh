30c2de8b9bd136857d9750487515e0d1ddd188a3
s-andrews <simon@proteo.me.uk>
2021-03-03 11:26:03 +0000

    Created config for bismark install

---
+++ programs/bismark.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo wget -O /opt/bismark.tar.gz https://github.com/FelixKrueger/Bismark/archive/0.23.0.tar.gz
+sudo tar -C /opt/ -xzf /opt/bismark.tar.gz
+sudo mv /opt/Bismark-0.23.0 /opt/bismark
+
+# There are too many bismark programs to link them all individually
+# so we just put the whole folder on the path
+
+sudo sh -c 'echo "export PATH=$PATH:/opt/bismark
+" > /etc/profile.d/bismark.sh'
+
