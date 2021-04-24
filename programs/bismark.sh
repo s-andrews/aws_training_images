51c3bd43553d0ec838526ec12a066e3b677849e2
Simon Andrews <simon@proteo.me.uk>
2021-04-24 10:41:32 +0100

    Use bash.bashrc for PATH changes.

--- programs/bismark.sh
+++ programs/bismark.sh
@@ -4,8 +4,10 @@ sudo tar -C /opt/ -xzf /opt/bismark.tar.gz
 sudo mv /opt/Bismark-0.23.0 /opt/bismark
 
 # There are too many bismark programs to link them all individually
-# so we just put the whole folder on the path
+# so we just put the whole folder on the path.  We have to use
+# /etc/bash.bashrc rather than /etc/profile.d/ since profile.d only
+# works for login shells, and gnome bash terminals don't count
 
 sudo sh -c 'echo "export PATH=\$PATH:/opt/bismark
-" > /etc/profile.d/bismark.sh'
+" >> /etc/bash.bashrc'
 
