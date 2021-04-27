2a3b681e19f532d0dd48e7e4cf9f1c5805c837ef
Simon Andrews <simon@proteo.me.uk>
2021-04-27 08:08:47 +0100

    Change to using the smaller Meth course data file

--- methylation_ubuntu_20.04.sh
+++ methylation_ubuntu_20.04.sh
@@ -14,7 +14,7 @@
 ./programs/seqmonk.sh
 
 # Install course data
-sudo wget -O ~student/meth_data.tar http://www.bioinformatics.babraham.ac.uk/training/Methylation_Course/Meth_Course_Data.tar
+sudo wget -O ~student/meth_data.tar http://www.bioinformatics.babraham.ac.uk/training/Methylation_Course/Meth_Course_Data_Small.tar
 sudo tar -C ~student/ -xf ~student/meth_data.tar
 sudo chown -R student:student ~student/Meth_Course_Data
 sudo rm -f ~student/meth_data.tar
