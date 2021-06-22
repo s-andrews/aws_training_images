09161f8c62afbfed12715265d303550587762eac
Simon Andrews <simon@proteo.me.uk>
2021-06-22 08:23:36 +0100

    Add evince to figure design

--- figdesign_ubuntu_20.04.sh
+++ figdesign_ubuntu_20.04.sh
@@ -3,6 +3,9 @@
 # Start from the guacamole desktop
 ./guacamole_desktop_ubuntu_20.04.sh
 
+# Install evince to view PDFs
+sudo apt -y install evince
+
 # Install Inkscape
 sudo apt -y install inkscape
 
