b55120a4c8367c22affd20fecd49811996fa1acf
Simon Andrews <simon@proteo.me.uk>
2021-07-01 11:24:42 +0100

    Create fcsalayzer.sh

---
+++ programs/fcsalayzer.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo apt -y install openjdk-11-jdk
+sudo mkdir /opt/fcsalyzer
+sudo wget -O /opt/fcsalyzer/fcsalyzer.zip https://deac-ams.dl.sourceforge.net/project/fcsalyzer/Version%200.9.22-alpha/FCSalyzer%200.9.22-alpha.zip
+sudo unzip -d /opt/fcsalyzer /opt/fcsalyzer/fcsalyzer.zip
+
+sudo sh -c 'echo "#!/bin/bash
+java -jar /opt/fcsalyzer/FCSalyzer.jar" > /usr/local/bin/fcsalyzer'
+
+sudo chmod 755 /usr/local/bin/fcsalyzer
