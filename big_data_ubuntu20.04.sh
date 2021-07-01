c53bf1963aa305b7d35c79dcd4f48bb490b0db4e
Simon Andrews <simon@proteo.me.uk>
2021-07-01 11:33:29 +0100

    Create big_data_ubuntu20.04.sh

---
+++ big_data_ubuntu20.04.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Start from the guacamole desktop
+./guacamole_desktop_ubuntu_20.04.sh
+
+# Install programs
+./programs/r.sh
+./programs/fastqc.sh
+./programs/seqmonk.sh
+./programs/igv.sh
+./programs/fcsalyzer.sh
+
+# Remove xarchiver since if people double clicked on a big
+# gzipped file it would hang the server.
+
+sudo apt -y remove xarchiver
+
+# Install course data
+sudo wget --quiet -O ~student/bigdata.tar.gz http://www.bioinformatics.babraham.ac.uk/training/bigdata/bigdata.tar.gz
+sudo tar -C ~student/ -xf ~student/bigdata.tar.gz
+sudo chown -R student:student ~student/Big_Data
+sudo rm -f ~student/bigdata.tar.gz
