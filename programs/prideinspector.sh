6c878e2da0483513b59b17767c5a9bc5eb51fb31
Simon Andrews <simon@proteo.me.uk>
2021-07-01 16:33:04 +0100

    Update prideinspector.sh

--- programs/prideinspector.sh
+++ programs/prideinspector.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+# We're going to assume that java has already been installed.
 sudo mkdir /opt/prideinspector
-sudo wget -O /opt/prideinspector/pride.zip 
+sudo wget -O /opt/prideinspector/pride.zip http://ftp.pride.ebi.ac.uk/pride/resources/tools/inspector/latest/desktop/pride-inspector.zip
 sudo unzip -d /opt/prideinspector /opt/prideinspector/pride.zip
 
+# The version number here might change which is annoying.
 sudo sh -c 'echo "#!/bin/bash
 java -jar /opt/prideinspector/pride-inspector-2.5.4.jar" > /usr/local/bin/prideinspector'
 
