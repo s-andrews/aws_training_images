25a50bcb80214890f0eb5c9cde0c19d2bfc2e12d
Simon Andrews <simon@proteo.me.uk>
2021-07-07 17:09:05 +0100

    Add R data

--- big_data_ubuntu20.04.sh
+++ big_data_ubuntu20.04.sh
@@ -27,12 +27,19 @@ sudo tar -C ~student/ -xf ~student/bigdata.tar.gz
 sudo chown -R student:student ~student/Big_Data
 sudo rm -f ~student/bigdata.tar.gz
 
-# Install course data
+# Install unix course data
 sudo wget -O ~student/unix_intro_data.tar.gz http://www.bioinformatics.babraham.ac.uk/training/Introduction%20to%20Unix/unix_intro_data.tar.gz
 sudo tar -C ~student/ -xf ~student/unix_intro_data.tar.gz
 sudo chown -R student:student ~student/FastQ_Data
 sudo chown -R student:student ~student/seqmonk_genomes
 sudo rm -f ~student/unix_intro_data.tar.gz
+
+# Install R intro course data
+sudo yum -y install wget
+sudo wget -O ~student/rintro.zip https://www.bioinformatics.babraham.ac.uk/training/Introduction_to_R_tidyverse/R_tidyverse_intro_data.zip
+sudo unzip -d ~student/ ~student/rintro.zip
+sudo chown -R student:student ~student/R_tidyverse_intro_data
+sudo rm -f ~student/rintro.zip
 
 # Install tidyverse
 sudo Rscript -e "install.packages('tidyverse', repos='http://cloud.r-project.org')"
