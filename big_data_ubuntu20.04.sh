ef05f0a339f92b93c90aba24364fd98093ce3477
Simon Andrews <simon@proteo.me.uk>
2021-07-08 08:39:20 +0100

    Fix rstudio deb link

--- big_data_ubuntu20.04.sh
+++ big_data_ubuntu20.04.sh
@@ -17,8 +17,8 @@ sudo apt -y install xcowsay
 
 # Install RStudio
 sudo apt-get install gdebi-core
-wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-1.4.1717-amd64.deb
-sudo gdebi rstudio-server-1.4.1717-amd64.deb
+wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.4.1717-amd64.deb
+sudo gdebi rstudio-1.4.1717-amd64.deb
 
 
 # Install course data
