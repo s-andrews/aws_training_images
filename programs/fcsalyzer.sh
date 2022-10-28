05ee406378f2448b212f68f4bb6c1b114f2655e9
Simon Andrews <simon@proteo.me.uk>
2022-10-28 11:50:20 +0100

    Update download link

--- programs/fcsalyzer.sh
+++ programs/fcsalyzer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt -y install openjdk-11-jdk
 sudo mkdir /opt/fcsalyzer
-sudo wget -O /opt/fcsalyzer/fcsalyzer.zip https://deac-ams.dl.sourceforge.net/project/fcsalyzer/Version%200.9.22-alpha/FCSalyzer%200.9.22-alpha.zip
+sudo wget -O /opt/fcsalyzer/fcsalyzer.zip https://altushost-swe.dl.sourceforge.net/project/fcsalyzer/Version%200.9.22-alpha/FCSalyzer%200.9.22-alpha.zip
 sudo unzip -d /opt/fcsalyzer /opt/fcsalyzer/fcsalyzer.zip
 
 sudo sh -c 'echo "#!/bin/bash
