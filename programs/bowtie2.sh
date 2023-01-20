7619d4fe6d2eedbd631b228a3d91f1b165e12ca1
Simon Andrews <simon@proteo.me.uk>
2023-01-20 17:07:11 +0000

    Fix bowtie2 mirror as sourceforge isn't working

--- programs/bowtie2.sh
+++ programs/bowtie2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export TIMESTAMP=$(date +%s)
-sudo wget -O /opt/bowtie2.zip https://sourceforge.net/projects/bowtie-bio/files/bowtie2/2.4.2/bowtie2-2.4.2-linux-x86_64.zip/download?ts=$TIMESTAMP\&use_mirror=autoselect
+sudo wget -O /opt/bowtie2.zip https://kumisystems.dl.sourceforge.net/project/bowtie-bio/bowtie2/2.4.2/bowtie2-2.4.2-linux-x86_64.zip
 sudo unzip -d /opt/ /opt/bowtie2.zip
 sudo mv /opt/bowtie2-2.4.2-linux-x86_64 /opt/bowtie2
 sudo ln -s /opt/bowtie2/bowtie2 /usr/local/bin/
