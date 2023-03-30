8cc75611c8e6b3a6c371efdab8c8b608c676260d
Simon Andrews <simon@proteo.me.uk>
2023-03-30 10:36:46 +0100

    Update to the new release of fastqc

--- programs/fastqc.sh
+++ programs/fastqc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt -y install openjdk-11-jdk
-sudo wget -O /opt/fastqc.zip http://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.9.zip 
+sudo wget -O /opt/fastqc.zip https://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.12.1.zip 
 sudo unzip -d /opt/ /opt/fastqc.zip
 sudo chmod 755 /opt/FastQC/fastqc
 sudo ln -s /opt/FastQC/fastqc /usr/local/bin
