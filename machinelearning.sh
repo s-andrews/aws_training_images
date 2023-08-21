3a2abb952f18f42d7a2fc4ddc98ccc5ac9e2e2be
s-andrews <simon@proteo.me.uk>
2023-08-21 11:05:59 +0100

    Fixed R package and data installs for ML

--- machinelearning.sh
+++ machinelearning.sh
@@ -11,9 +11,10 @@ sudo /usr/local/bin/Rscript -e "install.packages('tidymodels', repos='https://cl
 sudo /usr/local/bin/Rscript -e "install.packages('ranger', repos='https://cloud.r-project.org')"
 sudo /usr/local/bin/Rscript -e "install.packages('kknn', repos='https://cloud.r-project.org')"
 sudo /usr/local/bin/Rscript -e "install.packages('brulee', repos='https://cloud.r-project.org')"
+sudo /usr/local/bin/Rscript -e "torch::install_torch()"
 
 # Install intro course data
-sudo wget -O ~student/mldata.zip https://www.bioinformatics.babraham.ac.uk/training/Introduction_to_Machine_Learning/MachineLearningData.zip
+sudo wget -O ~student/mldata.zip http://www.bioinformatics.babraham.ac.uk/training/MachineLearning/MachineLearningData.zip
 sudo unzip -d ~student/ ~student/mldata.zip
 sudo chown -R student:student ~student/MachineLearningData
 sudo rm -f ~student/mldata.zip
