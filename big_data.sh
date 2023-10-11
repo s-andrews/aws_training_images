393ef2b9881b19ad6f5ec2991debc3f699b979b8
Simon Andrews <simon@proteo.me.uk>
2023-10-11 14:20:23 +0100

    Updated rstudio version

--- big_data.sh
+++ big_data.sh
@@ -17,8 +17,8 @@ sudo apt -y install vlc
 
 # Install RStudio
 sudo apt -y install gdebi-core
-sudo wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-2022.07.2-576-amd64.deb
-sudo gdebi -n rstudio-2022.07.2-576-amd64.deb
+sudo wget https://download1.rstudio.org/electron/jammy/amd64/rstudio-2023.09.0-463-amd64.deb
+sudo gdebi -n rstudio-2023.09.0-463-amd64.deb
 
 # Install Thonny
 sudo apt -y install thonny
