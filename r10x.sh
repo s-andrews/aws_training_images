6a9f63b01157353ac7ffeb17d0b3d8c157d942a8
s-andrews <simon@proteo.me.uk>
2024-01-24 16:16:09 +0000

    Remove sleepwalk since it doesn't work properly anyway

--- r10x.sh
+++ r10x.sh
@@ -16,16 +16,13 @@ sudo dnf -y install libpng-devel
 sudo dnf -y install hdf5-devel
 # Needed for seurat
 sudo dnf -y install geos-devel
-
-
-# The version of the hdf5 library on centos7 means we can't install anything newer than 1.0.0 in this package
+# Hdf5 is needed to read cellranger files
 sudo /usr/local/bin/Rscript -e "install.packages('hdf5r', repos='https://cloud.r-project.org')"
 
 # We shouldn't need to do this but seurat seems to fail when shiny isn't already installed
 # so we'll manually add that first.
 sudo /usr/local/bin/Rscript -e "install.packages('shiny', repos='https://cloud.r-project.org')"
 sudo /usr/local/bin/Rscript -e "install.packages('Seurat', repos='https://cloud.r-project.org')"
-sudo /usr/local/bin/Rscript -e "install.packages('sleepwalk', repos='https://cloud.r-project.org')"
 sudo /usr/local/bin/Rscript -e "install.packages('SCINA', repos='https://cloud.r-project.org')"
 
 
