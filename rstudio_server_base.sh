3ddf7bc96a46267e2eb462b01536e319bedc2999
Simon Andrews <simon@proteo.me.uk>
2026-03-23 21:00:27 +0000

    Add cmake to rstudio base

--- rstudio_server_base.sh
+++ rstudio_server_base.sh
@@ -117,7 +117,7 @@ sudo systemctl start httpd
 
 # We should install some basic R packages which everything will need. This also means
 # installing some OS packages which are needed to build them.
-sudo dnf -y install libxml2-devel openssl-devel fontconfig-devel harfbuzz-devel fribidi-devel  freetype-devel libpng-devel libtiff-devel libjpeg-turbo-devel
+sudo dnf -y install libxml2-devel openssl-devel fontconfig-devel harfbuzz-devel fribidi-devel  freetype-devel libpng-devel libtiff-devel libjpeg-turbo-devel cmake
 sudo /usr/local/bin/Rscript -e "install.packages('tidyverse', repos='https://cloud.r-project.org')"
 
 # For downloads to work we need to install additional certificates
